8acf59411a618e826033632404f69312198a4add
Mateusz Chwast <chwastmat@gmail.com>
2024-07-15 23:51:00 +0200

    add unimplemented free space search

--- Core/URTOS/Memory/urtos_memory.c
+++ Core/URTOS/Memory/urtos_memory.c
@@ -3,19 +3,41 @@
 #include "urtos_config.h"
 
 
-typedef struct {
+#define MEMORY_BLOCK_EMPTY_ADDR		0xFFFF
+
+
+struct URTOS_Memory_BlockHeader {
 	uint16_t blockSize;
 	uint16_t nextBlockIndex;
-} URTOS_Memory_BlockHeader;
+};
 
+typedef struct URTOS_Memory_BlockHeader BlockHeader;
 
 static uint16_t firstBlockIndex;
 
 static uint8_t memory[URTOS_CONFIG_MEMORY_ALLOCATED_SIZE];
 
 
+static inline uint16_t GetNextMemoryBlock(uint16_t previousBlock) {
+	BlockHeader* prev = (BlockHeader*)&memory[previousBlock];
+	uint16_t nextBlock = prev->nextBlockIndex;
+	return nextBlock;
+}
+
+static uint16_t GetNextFreeSpace(uint16_t startBlock, uint16_t space) {
+	uint16_t freeSpaceAddr = MEMORY_BLOCK_EMPTY_ADDR;
+	uint16_t currentBlock = startBlock;
+
+	while(currentBlock != MEMORY_BLOCK_EMPTY_ADDR) {
+
+	}
+
+	return freeSpaceAddr;
+}
+
 void* URTOS_Memory_Allocate(uint16_t bytesToAllocate) {
 	if(bytesToAllocate == 0) {
 		return NULL;
 	}
+
 }
