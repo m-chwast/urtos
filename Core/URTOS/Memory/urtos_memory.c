1cb5fd59cc2d59bd17bc9691b24295b1e2ef721b
Mateusz Chwast <chwastmat@gmail.com>
2024-07-28 14:01:35 +0200

    add custon free space search algorithm

--- Core/URTOS/Memory/urtos_memory.c
+++ Core/URTOS/Memory/urtos_memory.c
@@ -25,6 +25,28 @@ static inline BlockHeader* GetNextMemoryBlock(BlockHeader* previousBlock) {
 	return nextBlock;
 }
 
+/*
+ * Free space search algorithm:
+ *
+ * 1. Initialize current block and last block to start block
+ * 2. Find the block that is closest to the current block from the right side:
+ * 		example memory structure: |---A-C--ED--B-|, where A - current block;
+ * 		executing the step will set last block to C
+ *
+ * 		Now we know that between A and C there is NO OTHER BLOCK - space between is empty and can be allocated.
+ * 		We also know that for any other block on the right there is at least one allocated block (C), so we can't treat that as empty space.
+ *
+ *		If no block is found on the right, treat right memory border as the right block.
+ *
+ * 	3. Check the empty space size and perform the allocation if space is sufficient - "glue" the new block to right side of A - DONE
+ * 	4. If space was not sufficient, set current block to its next block
+ * 	5. If current block is not NULL then GOTO step 2
+ * 	6. Else find leftmost block and check space between left memory border and found block
+ * 	7. If able, allocate at the left memory border - DONE
+ * 	8. Else - FAIL - no empty space with sufficient capacity
+ *
+ */
+
 static BlockAllocator GetNextFreeSpace(const BlockHeader* startBlock, uint16_t space) {
 	// TODO
 	BlockHeader* freeSpaceAddr = NULL;
