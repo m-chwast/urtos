724cf0000225b21b74b58518447408e946eb3e1e
Mateusz Chwast <chwastmat@gmail.com>
2024-08-05 22:24:58 +0200

    test first allocation

--- Tests/URTOS/Memory/memory_allocate_tests.cpp
+++ Tests/URTOS/Memory/memory_allocate_tests.cpp
@@ -23,3 +23,33 @@ TEST_F(MemoryAllocateTests, AllocatingTooLargeBlockAtStartupFails) {
 	EXPECT_EQ(res, nullptr);
 	EXPECT_EQ(firstBlock, nullptr);
 }
+
+TEST_F(MemoryAllocateTests, FirstAllocationWorks) {
+	void* res = URTOS_Memory_Allocate(4);
+
+	EXPECT_NE(res, nullptr);
+	EXPECT_EQ((uint8_t*)firstBlock, memory);
+	EXPECT_EQ(res, &memory[8]);
+	EXPECT_EQ(firstBlock->blockSize, 4);
+}
+
+TEST_F(MemoryAllocateTests, AllocationOfNon4MultipleExtendsSize) {
+	URTOS_Memory_Allocate(5);
+
+	EXPECT_NE(firstBlock, nullptr);
+	EXPECT_EQ(firstBlock->blockSize, 8);
+}
+
+TEST_F(MemoryAllocateTests, AllocationOfNon4MultipleExtendsSize2) {
+	URTOS_Memory_Allocate(7);
+
+	EXPECT_NE(firstBlock, nullptr);
+	EXPECT_EQ(firstBlock->blockSize, 8);
+}
+
+TEST_F(MemoryAllocateTests, AllocationOfNon4MultipleExtendsSize3) {
+	URTOS_Memory_Allocate(10);
+
+	EXPECT_NE(firstBlock, nullptr);
+	EXPECT_EQ(firstBlock->blockSize, 12);
+}
