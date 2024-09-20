9667998bd5961d6fb63f21a4a4dfc1917e568d78
Mateusz Chwast <chwastmat@gmail.com>
2024-09-20 21:56:21 +0200

    add test for free with null pointer

--- Tests/URTOS/Memory/memory_free_tests.cpp
+++ Tests/URTOS/Memory/memory_free_tests.cpp
@@ -12,3 +12,26 @@ class MemoryFreeTests : public testing::Test {
 		firstBlock = NULL;
 	}
 };
+
+TEST_F(MemoryFreeTests, DoesNothingOnNullPointer) {
+	// allocate some initial values
+	URTOS_Memory_Allocate(1);
+	URTOS_Memory_Allocate(1);
+	// store current memory state
+	std::array<uint8_t, sizeof(memory)> initialMemory;
+	for(unsigned i = 0; i < sizeof(memory); i++) {
+		initialMemory[i] = memory[i];
+	}
+	BlockHeader* initialFirstBlock = firstBlock;
+
+	// free null pointer
+	URTOS_Memory_Free(NULL);
+
+	std::array<uint8_t, sizeof(memory)> currentMemory;
+	for(unsigned i = 0; i < sizeof(memory); i++) {
+		currentMemory[i] = memory[i];
+	}
+
+	EXPECT_EQ(initialFirstBlock, firstBlock);
+	EXPECT_EQ(initialMemory, currentMemory);
+}
